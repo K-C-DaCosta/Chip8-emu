24ce571e7cdf0882a21d7aca36f1960e5d095944
k-c-dacosta <khadeem.dacosta@gmail.com>
2022-12-08 12:14:06 -0500

    Starting work on Emulator
    Looking up the specs of the platform on google I found that the CPU
    has 16 8-bit registers. a 64 byte stack. Among other things...
    
    Accodrind the spec the frame buffer is has a bit-depth of 1 so I'm
    going to write an extremely compact framebuffer that can fit in cache
    of any modern CPU snugly.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,18 @@
+pub struct CompactFrameBuffer{
+    
+}
+
+pub struct Cpu{
+    registers:[u8;16],
+    index_register:u16,
+    stack:[u8;64],
+    delay_timer:u8,
+    sound_timer:u8,
+    frame_buffer:[u64;32]
+}
+
+
+
 fn main() {
     println!("Hello, world!");
 }
